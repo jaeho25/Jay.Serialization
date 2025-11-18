2efa31294380afedb75ce3aa319dbb0e74e64d34
Jaeho Lee <jaeho25@gmail.com>
2025-11-19 08:16:51 +0900

    초기 커밋.

---
+++ Src/Jay.Serialization/SerializableObjectWriter.cs
@@ -0,0 +1,80 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jay.Serialization
+{
+    internal class SerializableObjectWriter : JsonConverter
+    {
+        private readonly Dictionary<object, string> _objectToId = new(ReferenceEqualityComparer.Instance);
+        private int _nextId = 1;
+
+        public override bool CanWrite => true;
+        public override bool CanRead => false;
+
+        public override bool CanConvert(Type objectType)
+        {
+            return typeof(ISerializableObject).IsAssignableFrom(objectType);
+        }
+
+        public override void WriteJson(JsonWriter writer, object? value, JsonSerializer serializer)
+        {
+            if (value == null)
+            {
+                writer.WriteNull();
+                return;
+            }
+
+            if (value is not ISerializableObject)
+            {
+                throw new ArgumentException("It has to be ISerializableObject", nameof(value));
+            }
+
+            // 이미 직렬화된 객체면 $ref
+            if (_objectToId.TryGetValue(value, out var existingId))
+            {
+                writer.WriteStartObject();
+                writer.WritePropertyName("$ref");
+                writer.WriteValue(existingId);
+                writer.WriteEndObject();
+                return;
+            }
+
+            // 새 객체 등록
+            string newId = "_" + (_nextId++).ToString();
+            _objectToId[value] = newId;
+
+            var type = value.GetType();
+
+            writer.WriteStartObject();
+
+            writer.WritePropertyName("$id");
+            writer.WriteValue(newId);
+
+            writer.WritePropertyName("$type");
+
+            //버전 정보 없이 저장
+            //writer.WriteValue(type.AssemblyQualifiedName);
+            writer.WriteValue(Serializer.GetTypeName(type));
+
+            var info = new SerializableObjectInfo();
+            ((ISerializableObject)value).GetObjectData(info);
+
+            foreach (var kv in info.Values)
+            {
+                writer.WritePropertyName(kv.Key);
+                serializer.Serialize(writer, kv.Value);
+            }
+
+            writer.WriteEndObject();
+        }
+
+        public override object? ReadJson(JsonReader reader, Type objectType, object? existingValue, JsonSerializer serializer)
+        {
+            throw new InvalidOperationException("This is the Writer");
+        }
+    }
+}
---
+++ Src/TestConsole/Program.cs
@@ -0,0 +1,113 @@
+﻿using Jay.Serialization;
+
+namespace TestConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var fruits = new Category() { Name = "Fruits" };
+            var coffee = new Category() { Name = "Coffee" };
+
+            Store store = new Store();
+            store.Categories.Add(fruits);
+            store.Categories.Add(coffee);
+
+            Product apple = new Product() { Name = "Apple", Category = fruits };
+            Product banana = new Product() { Name = "Banana", Category = fruits };
+
+            store.Products.Add(apple);
+            store.Products.Add(banana);
+
+            Product americano = new Product() { Name = "Americano", Category = coffee };
+            Product moca = new Product() { Name = "Moca", Category = coffee };
+            
+            store.Products.Add(americano);
+            store.Products.Add(moca);
+
+            string json = Jay.Serialization.Serializer.Serialize(store);
+
+            Store store2 = Jay.Serialization.Serializer.Deserialize<Store>(json);
+
+            foreach (var category in store2.Categories)
+            {
+                Console.WriteLine($"{category.Name}");
+            }
+
+            foreach (var product in store2.Products)
+            {
+                Console.WriteLine($"{product.Category.Name}, {product.Name}");
+            }
+
+            Console.WriteLine(store2.Products[0].Category == store2.Products[1].Category);
+        }
+    }
+
+    internal class Store : ISerializableObject
+    {
+        public List<Category> Categories { get; set; } = new List<Category>();
+        public List<Product> Products { get; set; } = new List<Product>();
+
+        //requires empty constructor
+        public Store()
+        {
+            
+        }
+
+        public void GetObjectData(SerializableObjectInfo info)
+        {
+            info.AddValue(nameof(Categories), Categories);
+            info.AddValue(nameof(Products), Products);
+        }
+        public void SetObjectData(SerializableObjectInfo info)
+        {
+            Categories = info.GetValue<List<Category>>(nameof(Categories))!;
+            Products = info.GetValue<List<Product>>(nameof(Products))!;
+        }
+    }
+
+    internal class Category : ISerializableObject
+    {
+        public string Name { get; set; } = null!;
+
+        //requires empty constructor
+        public Category()
+        {
+            
+        }
+
+        public void GetObjectData(SerializableObjectInfo info)
+        {
+            info.AddValue(nameof(Name), Name);
+        }
+
+        public void SetObjectData(SerializableObjectInfo info)
+        {
+            Name = info.GetValue<string>(nameof(Name))!;
+        }
+    }
+
+    internal class Product : ISerializableObject
+    {
+        public string Name { get; set; } = null!;
+
+        public Category Category { get; set; } = null!;
+
+        //requires empty constructor
+        public Product()
+        {
+            
+        }
+        public void GetObjectData(SerializableObjectInfo info)
+        {
+            info.AddValue(nameof(Name), Name);
+            info.AddValue(nameof(Category), Category);
+        }
+
+        public void SetObjectData(SerializableObjectInfo info)
+        {
+            Name = info.GetValue<string>(nameof(Name))!;
+            Category = info.GetValue<Category>(nameof(Category))!;
+        }
+    }
+}
